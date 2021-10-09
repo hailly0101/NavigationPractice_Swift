5afe73b3fb761706815a29081aaa06c0e94024ea
haejii <yanghagi@naver.com>
2021-10-09 12:16:34 +0900

    Make segue push view controller

---
+++ ScreenTransitionExample/SeguePushViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SeguePushViewController.swift
+//  ScreenTransitionExample
+//
+//  Created by 양혜지 on 2021/10/09.
+//
+
+import UIKit
+
+class SeguePushViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}

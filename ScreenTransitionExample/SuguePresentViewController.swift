feb29d402cc34d12235d11f50db7b596322a089d
haejii <yanghagi@naver.com>
2021-10-10 11:35:18 +0900

    Present Set Button

---
+++ ScreenTransitionExample/SuguePresentViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SuguePresentViewController.swift
+//  ScreenTransitionExample
+//
+//  Created by 양혜지 on 2021/10/10.
+//
+
+import UIKit
+
+class SuguePresentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func tabBackButton(_ sender: UIButton) {
+        self.presentingViewController?
+            .dismiss(animated: true, completion: nil)
+    }
+    
+}

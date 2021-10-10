b380467d870d2fca75c816d55af6e872aaa04653
haejii <yanghagi@naver.com>
2021-10-10 11:59:57 +0900

    Make Code Present Button

---
+++ ScreenTransitionExample/CodePresentViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CodePresentViewController.swift
+//  ScreenTransitionExample
+//
+//  Created by 양혜지 on 2021/10/10.
+//
+
+import UIKit
+
+class CodePresentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func tapBackButton(_ sender: UIButton) {
+        self.presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+}
--- ScreenTransitionExample/ViewController.swift
+++ ScreenTransitionExample/ViewController.swift
@@ -19,5 +19,11 @@ class ViewController: UIViewController {
         self.navigationController?.pushViewController(viewController, animated: true)
     }
     
+    @IBAction func tapCodePresentButton(_ sender: UIButton) {
+        guard let viewController =
+                self.storyboard?.instantiateViewController(identifier: "CodePresentViewController") else { return }
+        viewController.modalPresentationStyle = .fullScreen
+        self.present(viewController, animated: true, completion: nil)
+    }
 }
 
